2ff2297358d2b4f8a2909878450192e1c7007e13
André Knörig <andre.knoerig@gmail.com>
2019-05-01 22:01:27 +0200

    p0 task0

--- p0/Task0.py
+++ p0/Task0.py
@@ -20,3 +20,10 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+first_text = texts[ 0 ]
+last_call = calls[ -1 ]
+
+print( "First record of texts, {0} texts {1} at time {2}".format( 
+    first_text[0], first_text[1], first_text[2] ))
+print( "Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds".format( 
+    last_call[0], last_call[1], last_call[2], last_call[3] ))
