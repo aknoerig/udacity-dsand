72c9c1d182e14f15ed473502fdb306ecbd07c1f3
André Knörig <andre.knoerig@gmail.com>
2019-05-20 23:22:29 +0200

    project 1, problem 4

---
+++ p1/Problem4.py
@@ -0,0 +1,63 @@
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    if user in group.users:
+        return True
+    if len(group.groups) == 0:
+        return False
+    for subgroup in group.groups:
+        return is_user_in_group( user, subgroup )
+
+
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+parent_user = "parent_user"
+parent.add_user(parent_user)
+
+child_user = "child_user"
+child.add_user(child_user)
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+print( is_user_in_group( "parent_user", parent ) )
+print( is_user_in_group( "child_user", child ) )
+print( is_user_in_group( "sub_child_user", sub_child ) )
+
+print( is_user_in_group( "parent_user", child ) )
+print( is_user_in_group( "child_user", sub_child ) )
+
+print( is_user_in_group( "sub_child_user", child ) )
+print( is_user_in_group( "sub_child_user", parent ) )
